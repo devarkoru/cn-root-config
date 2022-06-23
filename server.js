fb685bec8b983c92890a0d3c501543342fe580d5
Claudio Núñez Oyarzún <arkrater@gmail.com>
2022-06-23 14:06:52 -0400

    pruebas cors

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ var app = express()
 
 app.use(cors())
 
+app.get('/portal', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
 app.use(express.static(path.join(__dirname, "dist")))
   .get("*", (req, res) => {
     res.sendFile("index.html", { root: "dist" });
