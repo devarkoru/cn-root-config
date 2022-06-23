0f4721aaa82887df6825dd431f4334d900704a8e
Claudio Núñez Oyarzún <arkrater@gmail.com>
2022-06-23 14:39:01 -0400

    pruebas cors 3

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require('cors');
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 var app = express()
 
 app.use(cors())
-
-app.listen(5000, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
 
 app.use(express.static(path.join(__dirname, "dist")))
   .get("*", (req, res) => {
