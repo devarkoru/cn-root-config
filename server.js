7f407de428c3fe8041f6e2a2033bc000ee08e632
Claudio Núñez Oyarzún <arkrater@gmail.com>
2022-06-22 21:47:58 -0400

    revert

--- server.js
+++ server.js
@@ -2,25 +2,6 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
-const whitelist = ['https://cn*'];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-      if (!origin) {
-          return callback(null, true);
-      }
-
-      if (whitelist.indexOf(origin) === -1) {
-          var msg = 'The CORS policy for this site does not ' +
-              'allow access from the specified Origin.';
-          return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-  }
-}
-
-use(cors(corsOptions));
-
 express()
   .use(express.static(path.join(__dirname, "dist")))
   .get("*", (req, res) => {
