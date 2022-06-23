5ead374a4983480f7a33ed41c302f708f2bff975
Claudio Núñez Oyarzún <arkrater@gmail.com>
2022-06-23 15:51:57 -0400

    prueba cors 4

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const cors = require('cors');
 const path = require("path");
 const PORT = process.env.PORT || 8080;
 
 var app = express()
 
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, "dist")))
   .get("*", (req, res) => {
-
+    
     res.sendFile("index.html", { root: "dist" });
 
   })
