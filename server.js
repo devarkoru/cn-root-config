b316b50e3a4d35d64be67ec3a88f06626426c52c
Claudio Núñez Oyarzún <arkrater@gmail.com>
2022-06-23 15:21:29 -0400

    prueba cors 4

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require('cors');
 const path = require("path");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 var app = express()
 
