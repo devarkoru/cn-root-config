40bd4dca56e217faf8ec484e5363ba1bd226c9d9
Claudio Núñez Oyarzún <arkrater@gmail.com>
2022-06-23 14:11:08 -0400

    prueba cors 3

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ var app = express()
 
 app.use(cors())
 
-app.get("*", function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
 app.listen(5000, function () {
   console.log('CORS-enabled web server listening on port 80')
 })
