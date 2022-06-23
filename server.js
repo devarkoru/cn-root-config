20944e2ccc43438cdeb2ab2f39854e56ee09556d
Claudio Núñez Oyarzún <arkrater@gmail.com>
2022-06-22 21:02:53 -0400

    prueba cors

--- server.js
+++ server.js
@@ -2,6 +2,25 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
+//Cors Configuration - Start
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
+//Cors Configuration - End
+
+
 express()
   .use(express.static(path.join(__dirname, "dist")))
   .get("*", (req, res) => {
